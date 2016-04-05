fe1052211302d7c53282a39127143fc9a09238f7
berziiii <brian@offpistedesigns.com>
2016-04-05 15:32:01 -0400

    adjusted video-library page to incorporate bootstrap and be responsive

--- src/js/video-library.js
+++ src/js/video-library.js
@@ -1,6 +1,7 @@
 var html = '';
 var links = [
-  {title: 'Money Matrix', category: 'Wealth &amp; Wisdom', url: '22syy9k6gt'},
+  {title: 'Money Matrix', category: 'Wealth &amp; Wisdom', url: 'lqxkubtzoy'},
+  {title: 'Transfers Of Wealth', category: 'Wealth &amp; Wisdom', url: 'a7jlsggdiv'},
   // {Title: ' Ski Coach.'},
   // {Title: ' Longboarder.'},
   // {Title: ' Problem-Solver.'},
@@ -14,12 +15,13 @@ function populateVideoList(link) {
 
   var ret = '';
 
-  ret += '<div class="video-wrapper">';
+  ret += '<div class="col-lg-6 col-md-6 col-sm-6 video-wrapper">';
   ret += '<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script>';
-  ret += '<h1>' + link.title + '</h1>';
-  ret += '<h3>' + link.category + '</h3>';
-  ret += '<div class="wistia_embed wistia_async_' + link.url + '" style="height:320px;width:426px;margin:0px auto">&nbsp;</div>';
+  ret += '<h3>' + link.title + '</h3>';
+  ret += '<h4>' + link.category + '</h4>';
+  ret += '<div class="wistia_embed wistia_async_' + link.url + '" style="height:320px;max-width:426px;margin:0px auto">&nbsp;</div>';
   ret += '<div class="video-link-container"><a class="video-library-back" href="index.html">Go Back</a></div>';
+  ret += '</div>';
 
   return ret;
 }
