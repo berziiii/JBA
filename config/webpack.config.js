0149bbd30a3df3b40db369e3d18c789b80a04e29
berziiii <berzelba@gmail.com>
2020-11-30 15:21:09 -0500

    webpack adjustments

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -748,6 +748,6 @@ module.exports = function (webpackEnv) {
     // Turn off performance processing because we utilize
     // our own hints via the FileSizeReporter
     performance: false,
-    externals: isEnvDevelopment ? {} : isBundleTypeLibrary ? [nodeExternals()] : [],
+    externals: isEnvDevelopment ? {} : [nodeExternals()],
   };
 };
