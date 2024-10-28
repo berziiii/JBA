beeabdcc5c5aa43a9ad9f85e0cbad30c2e60d07c
Brian Berzellini <berzelba@gmail.com>
2024-10-28 10:11:56 -0400

    add BBB logo to footer (#10)

--- src/web/components/footer/Footer.tsx
+++ src/web/components/footer/Footer.tsx
@@ -23,6 +23,14 @@ const Footer = observer(() => {
         onKeyPress={e => handleOnEnter(e, scrollToTop)}>
         Terms & Conditions
       </Link>
+      <a href="https://www.bbb.org/us/md/timonium/profile/financial-consultants/jba-financial-advisors-0011-90206830/#sealclick"
+        target="_blank"
+        rel="nofollow"
+        className={styles.bbbLink}>
+        <img src="https://seal-greatermd.bbb.org/seals/blue-seal-293-61-whitetxt-bbb-90206830.png"
+          style={{ border: 0 }}
+          alt="JBA Financial Advisors BBB Business Review" />
+      </a>
     </div>
   );
 });
